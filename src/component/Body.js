947320f9fff57d23d203b2283f21d86ab0b941e9
Bhoomika Sahu <bhoomikasahu0010@gmail.com>
2023-10-06 00:16:41 +0530

    API call and Shimmer UI

--- src/component/Body.js
+++ src/component/Body.js
@@ -1,25 +1,41 @@
+import { useEffect, useState } from "react";
 import { resObj } from "../utils/mockData";
 import ResCard from "./ResCard";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  let listOfRestaurants = [];
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  useEffect(() => {
+    fetchData();
+
+  }, []);
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.2599333&lng=77.412615&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      );
+      const result = await data.json();
+      setListOfRestaurants(result?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    };
+  if(!listOfRestaurants.length) {
+    return <Shimmer />
+  }
   return (
     <div className="body">
       <div className="filter-restaurants">
         <button
           className="filter-btn"
           onClick={() => {
-            listOfRestaurants = resObj.filter((res) => {
-              return res.info.rating.aggregate_rating > 4
-            });
-            console.log(listOfRestaurants);
+            const list = listOfRestaurants.filter(
+              (res) => res.info.avgRating > 4
+            );
+            setListOfRestaurants(list);
           }}
         >
           Top Rated Restaurants
         </button>
       </div>
       <div className="res-container">
-        {listOfRestaurants.map((res) => (
+        {listOfRestaurants && listOfRestaurants.map((res) => (
           <ResCard resObj={res} />
         ))}
         {/* <ResCard resObj={resObj[0]} /> */}
--- src/component/ResCard.js
+++ src/component/ResCard.js
@@ -1,12 +1,13 @@
 const ResCard = (props) => {
   const { info } = props.resObj;
+  // console.log(info,'info');
   return (
     <div className="res-card">
-      <img className="res-logo" src={info.image.url} />
+      <img className="res-logo" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/" + info.cloudinaryImageId} />
       <h1>{info.name}</h1>
-      <div>{info.rating.aggregate_rating}</div>
-      <div>{info.cuisine.map((c) => `${c.name}, `)}</div>
-      <div>{info.locality.address}</div>
+      <div>{info.avgRating}</div>
+      <div>{info.cuisines.map((c) => `${c}, `)}</div>
+      <div>{info.locality}</div>
     </div>
   );
 };
